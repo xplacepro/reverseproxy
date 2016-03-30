2ae5a17dc38c76bf30854ec138228bc9a19b42c9
Alex Sofin <sofin.moffin@gmail.com>
2016-03-30 14:52:05 +0300

    config file

--- runserver.go
+++ runserver.go
@@ -18,26 +18,23 @@ func Usage() {
 }
 
 func main() {
-	var User = flag.String("user", "", "Basic auth user")
-	var Password = flag.String("password", "", "Basic auth password")
-
-	var ClientUser = flag.String("client-user", "", "Basic auth user for dashboard client")
-	var ClientPassword = flag.String("client-password", "", "Basic auth user password for dashboard client")
-
-	var Listen = flag.String("listen", ":8080", "Interface and port to listen, :8080")
-
+	var ConfigPath = flag.String("config", "config.ini", "Path to configuration file")
 	flag.Parse()
 
-	if *User == "" || *Password == "" {
-		Usage()
+	config, err := rpc.ParseConfiguration(*ConfigPath)
+
+	if err != nil {
+		panic(err)
 	}
 
-	env := &rpc.Env{Auth: rpc.BasicAuthorization{*User, *Password}, ClientUser: *ClientUser, ClientPassword: *ClientPassword}
+	env := &rpc.Env{Auth: rpc.BasicAuthorization{config["auth.user"], config["auth.password"]},
+		ClientAuth: rpc.ClientBasicAuthorization{config["client_auth.user"], config["client_auth.password"]}}
+
 	r := mux.NewRouter()
 	r.StrictSlash(false)
 	r.Handle("/api/v1/domains", rpc.Handler{env, controllers.PostListDomainHandler}).Methods("POST")
 	r.Handle("/api/v1/domains/{domain:[a-zA-Z0-9-.]+}", rpc.Handler{env, controllers.DeleteDomainHandler}).Methods("DELETE")
 	http.Handle("/", r)
-	log.Printf("Started server on %s", *Listen)
-	http.ListenAndServe(*Listen, nil)
+	log.Printf("Started server on %s", config["listen"])
+	panic(http.ListenAndServe(config["listen"], nil))
 }
