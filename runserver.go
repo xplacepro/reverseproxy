01973355431a377a0f6d1c8d2eded7eac4c5c4ad
Alex Sofin <sofin.moffin@gmail.com>
2016-03-30 18:15:54 +0300

    reload env

--- runserver.go
+++ runserver.go
@@ -2,33 +2,43 @@ package main
 
 import (
 	"flag"
-	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/xplacepro/reverseproxy/controllers"
 	"github.com/xplacepro/rpc"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"syscall"
 )
 
-func Usage() {
-	fmt.Println("Usage:")
-	flag.PrintDefaults()
-	os.Exit(0)
+func ReloadEnv(env *rpc.Env, config map[string]string) {
+	env.Auth = rpc.BasicAuthorization{config["auth.user"], config["auth.password"]}
+	env.ClientAuth = rpc.ClientBasicAuthorization{config["client_auth.user"], config["client_auth.password"]}
 }
 
 func main() {
 	var ConfigPath = flag.String("config", "config.ini", "Path to configuration file")
 	flag.Parse()
 
-	config, err := rpc.ParseConfiguration(*ConfigPath)
+	var config map[string]string
 
-	if err != nil {
-		panic(err)
-	}
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, syscall.SIGHUP)
 
-	env := &rpc.Env{Auth: rpc.BasicAuthorization{config["auth.user"], config["auth.password"]},
-		ClientAuth: rpc.ClientBasicAuthorization{config["client_auth.user"], config["client_auth.password"]}}
+	env := &rpc.Env{}
+
+	go func() {
+		for sign := range c {
+			rpc.ParseConfiguration(*ConfigPath, &config)
+			log.Printf("Reloading configuration, %s\n", sign)
+			ReloadEnv(env, config)
+		}
+	}()
+
+	rpc.ParseConfiguration(*ConfigPath, &config)
+
+	ReloadEnv(env, config)
 
 	r := mux.NewRouter()
 	r.StrictSlash(false)
